51806d7e708c306688a6018ec02d05f35fa3d60a
korya <korya@github.com>
2015-02-28 17:32:58 -0500

    Add unaccent extension

--- test/simple/test.sh
+++ test/simple/test.sh
@@ -7,6 +7,5 @@ grep -q "^listen_addresses = '*'" /etc/postgresql/9.3/main/postgresql.conf
 tail -1 /etc/postgresql/9.3/main/pg_hba.conf | \
   grep -q "^host simple simplerole 0.0.0.0/0 trust$"
 
-diff -u /src/original-config.yml /src/original-config.yml
 diff -u <(sort /src/expected-config.yml) <(sort /src/config.yml)
-diff -u /src/expected-config.sql /src/config.sql
+diff -u <(sort /src/expected-config.sql) <(sort /src/config.sql)
