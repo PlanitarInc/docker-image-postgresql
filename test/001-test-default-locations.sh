e7e2e8acb3841e44fc6126e6ce462d5e714c1f97
korya <korya@github.com>
2015-03-02 15:24:09 -0500

    Add test cases for `hstore` and `plv8` extensions

--- test/001-test-default-locations.sh
+++ test/001-test-default-locations.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 
+set -e
+
 docker run -d --name postgres-default-ext planitar/postgres
+test_done() { docker rm -f postgres-default-ext; exit ${1:-0}; }
 
 sleep 3s
 
+# Check `citext` extension availability
+docker exec -t postgres-default-ext psql -U postgres -c '
+  CREATE TABLE verify_citext_is_available (a citext);
+' || test_done $?
+
+# Check `hstore` extension availability
+docker exec -t postgres-default-ext psql -U postgres -c '
+  CREATE TABLE verify_hstore_is_available (a hstore);
+' || test_done $?
+
+# Check `plv8` extension availability
 docker exec -t postgres-default-ext psql -U postgres -c '
   CREATE FUNCTION sq(a int) RETURNS int AS $$
     return a * a;
   $$ LANGUAGE plv8;
   SELECT sq(2);
-'
-res=$?
+' || test_done $?
 
-docker rm -f postgres-default-ext
-
-test "$res" -eq 0
+test_done
