c42532ba5098c61fe1abf5687b01e78459bc1719
korya <korya@github.com>
2015-03-04 23:34:24 -0500

    Use `trap` in tests

--- test.sh
+++ test.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-set -ex
+set -e
+
+cleanup() {
+  docker rm -f test-postgres
+}
+report() {
+  docker logs planitar/postgres
+  echo "Test failed..."
+}
+trap report ERR
+trap cleanup EXIT
+
 
 docker run -d --name test-postgres -v /dbdata/postgres planitar/postgres
-test_done() { docker rm -f test-postgres; exit ${1:-0}; }
 
 sleep 3s
 
 docker exec -t test-postgres psql -U postgres -c '
   SELECT 1+1;
-' || test_done $?
+'
 
 docker run -t --rm --link test-postgres:postgres planitar/postgres bash -c '
   psql -h $POSTGRES_PORT_5432_TCP_ADDR -U postgres -c "SELECT 1+1";
-' || test_done $?
-
-test_done
+'
