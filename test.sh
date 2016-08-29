88da575ad4563f03aca2d5d37a5b0a917f1dc15e
korya <korya@github.com>
2016-08-29 09:17:43 -0400

    test: remove `-t` flag from containers running in the test

--- test.sh
+++ test.sh
@@ -17,10 +17,10 @@ docker run -d --name test-postgres -v /dbdata/postgres planitar/postgres
 
 sleep 3s
 
-docker exec -t test-postgres psql -U postgres -c '
+docker exec test-postgres psql -U postgres -c '
   SELECT 1+1;
 '
 
-docker run -t --rm --link test-postgres:postgres planitar/postgres bash -c '
+docker run --rm --link test-postgres:postgres planitar/postgres bash -c '
   psql -h $POSTGRES_PORT_5432_TCP_ADDR -U postgres -c "SELECT 1+1";
 '
